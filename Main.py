01f3038bf5455463bf2dabaf9531d84b559c2dc1
Deivid <167555701+DeividFilho@users.noreply.github.com>
2025-07-16 12:24:08 -0300

    16/07

--- Main.py
+++ Main.py
@@ -8,8 +8,6 @@ hub = PrimeHub()
 
 from funcoes_btf import andar_reto_suave, curva, reset, drive_base, a_motor, b_motor
 
-
-
 #Velocidade de Movimento e Curva
 
 velocidade_reta = 620
@@ -22,44 +20,41 @@ drive_base.settings(turn_rate=velocidade_reta)
 drive_base.settings(turn_acceleration=aceleracao_curva)
 drive_base.use_gyro(True)
 
-def M1_M2_M3():
+def saida_3():
+    """Missões M1: Tridente 2.0, M2: Grama, M3: Indianaa Jones"""
+    #Inicio da Missão M1: Tridente 2.0
     b_motor.run_target(1000, 0, wait=False)
     a_motor.run_angle(-1000, 550, wait=False)
-    andar_reto_suave(76, 350)
-    drive_base.turn(-95)
-    andar_reto_suave(10, 350)
+    andar_reto_suave(84, 250)
+    drive_base.turn(-97)
+    andar_reto_suave(15, 150)
     wait(200)
-    a_motor.run_angle(1000, 650)
+    a_motor.run_angle(1000, 1700)
+    wait(200)
+    a_motor.run_angle(-1000, 600)
+    wait(150)
     andar_reto_suave(15, -350)
     drive_base.turn(94)
     wait(100)
-    andar_reto_suave(28,350)
-    drive_base.turn(-55)
+    #Fim da Missão M1: Tridente 2.0 e Inicio da Missão M2: Grama
+    a_motor.run_angle(1000, 800, wait=False)
+    andar_reto_suave(19,350)
+    drive_base.turn(-54)
     wait(100)
-    andar_reto_suave(13,350)
+    andar_reto_suave(18,350)
     a_motor.run_angle(-1200, 650, wait=False)
     wait(200)
-    andar_reto_suave(16,-350)
+    andar_reto_suave(28,-350)
     wait(100)
-    drive_base.turn(67)
-    andar_reto_suave(10, 350)
-    b_motor.run_target(1000, 40)
-    andar_reto_suave(10,-250)
-    drive_base.turn(75)
-    andar_reto_suave(18,300)
-    drive_base.turn(70)
-    a_motor.run_angle(1200, 650)
-    andar_reto_suave(6,-300)
-    drive_base.turn(-70)
-    andar_reto_suave(20,-500)
     drive_base.turn(110)
-    andar_reto_suave(80, 350)
+    b_motor.run_angle(-1000, 110)
 
-def M12_areia():
+def saida_4():
+    """Missão M12: Vem e Vai"""
     b_motor.run_target(1000, 0, wait=False)
     andar_reto_suave(55, 200)
     b_motor.run_target(-1000, -115)
     wait(200)
     andar_reto_suave(65, -350)
 
-M1_M2_M3()
+saida_3()
