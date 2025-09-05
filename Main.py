a3312c84628d8da29aaa39e9986b5bbc97bd7bfc
Jean <jotaraiol7@gmail.com>
2025-09-05 12:16:45 -0300

    0509

--- Main.py
+++ Main.py
@@ -6,7 +6,7 @@ from pybricks.tools import wait, StopWatch, hub_menu
 from pybricks.tools import hub_menu
 hub = PrimeHub()
 
-from funcoes_btf import andar_reto_suave, curva, reset, drive_base, a_motor, b_motor,andar_reto,turn
+from funcoes_btf import andar_reto_suave, curva, reset, drive_base, a_motor, b_motor,andar_reto,turn, left_motor, right_motor
 
 #Velocidade de Movimento e Curva
 
@@ -29,19 +29,24 @@ def saida_2():
     """Missões M9: O que está à venda, M10: Desequilíbrio da Balança, M13: Reconstrução da Estátua"""
     #Inicio das Missões M9: O que está à venda, M10: Desequilíbrio da Balança
     b_motor.run_target(1000, 0, wait=False)
+    
+    andar_reto_suave(31,150)
+    drive_base.turn(44)
     a_motor.run_time(250,400, wait=False)
-    andar_reto_suave(23,150)
-    drive_base.turn(48)
-    andar_reto_suave(46,150)
+    andar_reto_suave(34,150)
     drive_base.turn(5)
     b_motor.run_time(-200, 1200)
     b_motor.hold()
-    a_motor.run_time(-200, 1150)
+    a_motor.run_time(-300, 1150)
     a_motor.run_time(600, 1900)
-    andar_reto_suave(17, -250)
+    andar_reto_suave(15, -350)
+    andar_reto_suave(3, 200)
     b_motor.run_target(-150, 0)
+    andar_reto_suave(5, -250)
     a_motor.run_time(200, 1700, wait=False)
-    drive_base.turn(-54)
+    b_motor.run_time(-200, 1200)
+    drive_base.turn(-50)
+    b_motor.run_target(-150, 0)
     andar_reto_suave(70, 350)
     drive_base.turn(25)
     andar_reto(25,200)
@@ -53,26 +58,29 @@ def saida_3():
     """Missões M1: Escavação Supercial, M2: Revelação do Mapa"""
     #Inicio da Missão M1: Escavação Supercial
     b_motor.run_target(300, -110, wait=False)
-    andar_reto_suave(92, 200)
-    drive_base.turn(-45)
-    andar_reto_suave(25, 100)
+    andar_reto_suave(101, 170)
+    drive_base.turn(-43)
+    andar_reto_suave(25, 60)
     b_motor.run_angle(250, 50)
     andar_reto(23,-250)
-    drive_base.turn(-18)
+    drive_base.turn(-19)
     a_motor.run_angle(-400, 500)
-    andar_reto(8, 100)
+    andar_reto(10, 100)
     a_motor.run_angle(400, 550)
-    andar_reto(11, -150)
+    andar_reto(13, -150)
     drive_base.turn(-125)
     andar_reto(60, 300)
 
 def saida_4():
     """Missão M12: Operação de Resgate"""
     #Inicio da Missão M12: Operação de Resgate
-    b_motor.run_target(1000, 0, wait=False)
-    andar_reto_suave(69, 200)
+    andar_reto_suave(47, 150)
+    andar_reto(15, 100)
     wait(100)
-    andar_reto(100, -250)
+    drive_base.turn(2)
+    andar_reto(25, -100)
+    andar_reto(13, 100)
+    andar_reto(90, -250)
 
 def saida_5():
     andar_reto_suave(135, 200)
