dc15a0c1f626613f65864f08e1ff9e3b142e4f4e
Jean <jotaraiol7@gmail.com>
2026-03-03 10:47:24 -0300

    V34

--- Main.py
+++ Main.py
@@ -20,10 +20,11 @@ def saida_1():
     
     andar_reto_suave(79,500)
     wait(400)
-    turn(-46.8, 100)
+    turn(-46.8, 125)
+
     print(hub.imu.heading())
     wait(800)
-    andar_reto_suave(9,300)
+    andar_reto_suave(9,340)
     andar_reto_suave(-3.5,150)
     b_motor.run_target(1000, -143)
     andar_reto_suave(4,700)
@@ -59,7 +60,7 @@ def saida_2():
     wait(250)
     
     b_motor.run_target(100, -140,wait=False)
-    andar_reto(4.3, 80)
+    andar_reto(4.35, 80)
 
     
     
@@ -113,8 +114,7 @@ def saida_4():
     andar_reto_suave(2, 500)
     print(hub.imu.heading())
     b_motor.run_target(390, -60)
-    
-    left_motor.run_angle(-200,30)
+    turn(5,200)
     andar_reto_suave(-8, 1000)
 
 
@@ -126,19 +126,22 @@ def saida_5():
     drive_base.reset()
     wait(100)
     drive_base.settings(turn_rate=200)
-    drive_base.curve(250,-90.8)
-    andar_reto(38.4, 300)
+    drive_base.curve(250,-89.7)  
+    print(hub.imu.heading())
+    andar_reto(38.8, 300)
+    print(hub.imu.heading())
+    turn(-89.9, 200)
+    andar_reto(8, 200)
+    right_motor.run_time(200,200)
+    a_motor.run_time(1000,2300)
+    right_motor.run_time(-200,200)
+    andar_reto(-7, 400)
+    turn(-91, 200)
+
+    andar_reto(10.9, 500)
     turn(-90, 200)
-    andar_reto(8.5, 200)
-    right_motor.run_time(200,260)
-    a_motor.run_time(1000,2400)
-    right_motor.run_time(-200,255)
-    andar_reto_suave(-7, 500)
-    turn(-90.5, 200)
-    andar_reto(10.65, 500)
-    b_motor.run_target(250, -40, wait=False)
-    turn(-88.5, 200)
-    andar_reto(6.5, 300)
+    andar_reto(6, 300)
+    andar_reto(-0.8, 500)
     b_motor.run_target(1000, -120)
     b_motor.run_target(500, -80, wait=False)
     andar_reto_suave(-8.5, 300)
@@ -147,9 +150,9 @@ def saida_5():
     b_motor.run_target(400, -135)
     andar_reto_suave(-8, 600)
     b_motor.run_target(500, 0, wait=False)
-    turn(60, 300)
+    turn(63, 300)
 
-    andar_reto_suave(76, 800)
+    andar_reto(75, 800)
    
 
 def saida_6():
@@ -182,7 +185,7 @@ def saida_7():
     drive_base.reset()
     wait(150)
     andar_reto(69.5, 450)
-    turn(-21, 300)
+    turn(-20.5, 300)
     andar_reto(9, 180)
     turn(-22, 800)
     andar_reto_suave(-7.5, 200)
@@ -230,7 +233,7 @@ def saida_8():
     drive_base.settings(turn_rate=200,turn_acceleration=150)
     drive_base.curve(-100,43.2)
    
-    andar_reto(74.2, 400)
+    andar_reto(74.4, 400)
     turn2(-70, 350)
     andar_reto(5, 500)
     b_motor.run_target(300, -120)
@@ -245,7 +248,7 @@ def saida_8():
     
     andar_reto(-10, 500)
     
-    turn(73,350)
+    turn(72,350)
     andar_reto(-52.4, 1000)
     turn(42,300)
     drive_base.settings(straight_acceleration=450,straight_speed=300)
@@ -254,6 +257,8 @@ def saida_8():
     wait(400)
 
     andar_reto(9, 1000)
+
+
 
 
 
---
+++ alianca.py
@@ -0,0 +1,96 @@
+from pybricks.hubs import PrimeHub
+from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
+from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
+from pybricks.robotics import DriveBase
+from pybricks.tools import wait, StopWatch
+
+hub = PrimeHub()
+
+# Funções para a FLL
+# Criadores: Deivid e Jean
+# Versão: 1.1
+# Funções: Andar Reto, Andar Reto com Rampa, Curva
+
+from pybricks.hubs import PrimeHub
+from pybricks.pupdevices import Motor, ColorSensor
+from pybricks.parameters import Direction, Port,Axis, Color
+from pybricks.tools import wait
+from pybricks.robotics import DriveBase
+
+
+hub = PrimeHub(top_side=Axis.Z, front_side=Axis.X)
+
+# Motores principais
+left_motor = Motor(Port.E, Direction.COUNTERCLOCKWISE)
+right_motor = Motor(Port.A)
+
+
+# DriveBase configurado
+drive_base = DriveBase(left_motor, right_motor, 60, 115)
+
+# Limites de controle dos motores
+
+velocidade_curva = 260
+aceleracao_curva = 455
+drive_base.settings(turn_rate=velocidade_curva)
+drive_base.settings(turn_acceleration=aceleracao_curva)
+graus_por_cm = 18.94
+drive_base.use_gyro(True)
+
+# ---------------- Funções auxiliares ----------------
+
+def reset():
+    """Reseta giroscópio e ângulo dos motores."""
+    hub.imu.reset_heading(0)
+    left_motor.reset_angle(0)
+    right_motor.reset_angle(0)
+
+def parar():
+    """Segura os motores principais."""
+    left_motor.hold()
+    right_motor.hold()
+
+
+
+# ---------------- Funções principais ----------------
+
+def andar_reto(cms, pot,wa=True):
+    """Anda reto em linha com velocidade constante."""
+    drive_base.stop()
+    reset()
+    drive_base.settings(straight_acceleration=450,straight_speed=pot)
+    drive_base.use_gyro(True)
+    
+    drive_base.straight(cms*10,wait=wa)
+
+
+def andar_reto_suave(cm, pot,wa=True):
+    """Anda reto com rampa de aceleração/desaceleração suave."""
+    drive_base.settings(straight_speed=pot)
+    drive_base.settings(straight_acceleration=400)
+    parar()
+    wait(100)
+    drive_base.use_gyro(True)
+    drive_base.straight(cm*10,wait=wa)
+    
+def turn(graus, potencia):    
+    velocidade_curva = potencia
+    aceleracao_curva = 300
+    parar()
+    wait(150)
+    drive_base.settings(turn_rate=velocidade_curva,turn_acceleration=aceleracao_curva)
+    drive_base.use_gyro(True)
+    drive_base.turn(graus)
+
+
+
+def turn2(graus, potencia):    
+    velocidade_curva = potencia
+    aceleracao_curva = 300
+    parar()
+    wait(150)
+    drive_base.settings(turn_rate=velocidade_curva,turn_acceleration=aceleracao_curva)
+    drive_base.use_gyro(True)
+    drive_base.turn(graus)
+
+turn(90,200)
