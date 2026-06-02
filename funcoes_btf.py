fc722cdfb1005b69e860834e44a548d35c88cca2
Jean <jotaraiol7@gmail.com>
2026-06-02 11:50:52 -0300

    OPEN V10

--- funcoes_btf.py
+++ funcoes_btf.py
@@ -83,19 +83,26 @@ def turn(graus, potencia):
 def turn2(graus, potencia):
     velocidade_curva = potencia
     aceleracao_curva = 300
+    tolerancia = 0.5
     parar()
     wait(150)
     drive_base.settings(turn_rate=velocidade_curva,turn_acceleration=aceleracao_curva)
+    
     if graus > hub.imu.heading():
         while hub.imu.heading() <= graus:
+            if abs(hub.imu.heading()) >= abs(graus):
+                break
             left_motor.dc(potencia)
             right_motor.dc(-potencia)
     elif graus == hub.imu.heading():
         pass
     else:
         while hub.imu.heading() >= graus:
+            if  abs(hub.imu.heading()) >= abs(graus):
+                break
             left_motor.dc(-potencia)
             right_motor.dc(potencia)
     drive_base.stop()
+
 
 
