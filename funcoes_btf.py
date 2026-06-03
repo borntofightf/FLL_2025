432c80592fcb6ab9ed9b904b8988fbc2374f3cf4
Deivid <167555701+DeividFilho@users.noreply.github.com>
2026-06-02 21:31:09 -0300

    OPEN V11

--- funcoes_btf.py
+++ funcoes_btf.py
@@ -83,19 +83,27 @@ def turn(graus, potencia):
 def turn2(graus, potencia):
     velocidade_curva = potencia
     aceleracao_curva = 300
+    timer = StopWatch()
     parar()
-    wait(150)
     drive_base.settings(turn_rate=velocidade_curva,turn_acceleration=aceleracao_curva)
+    wait(150)
+    timer.reset() 
     if graus > hub.imu.heading():
         while hub.imu.heading() <= graus:
+            if timer.time() >= 700 and abs(hub.imu.heading()) <= abs(graus)-0.5:
+                break
             left_motor.dc(potencia)
             right_motor.dc(-potencia)
     elif graus == hub.imu.heading():
         pass
     else:
         while hub.imu.heading() >= graus:
+            if timer.time() >= 700 and abs(hub.imu.heading()) <= abs(graus)-0.5:
+                break
             left_motor.dc(-potencia)
             right_motor.dc(potencia)
     drive_base.stop()
+    timer.pause()
+
 
 
